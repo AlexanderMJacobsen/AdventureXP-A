df09f7ac650997ffabbe56152ad828b37ed9cfc3
Alexander <alja0004@stud.ek.dk>
2026-03-04 09:48:14 +0100

    getting main up to date with project

--- src/main/java/com/xpgruppe/AdventureXP/controller/ActivityDetailsController.java
+++ src/main/java/com/xpgruppe/AdventureXP/controller/ActivityDetailsController.java
@@ -1,13 +1,20 @@
 package com.xpgruppe.AdventureXP.controller;
+import com.xpgruppe.AdventureXP.service.ActivityService;
+import com.xpgruppe.AdventureXP.service.EquipmentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ActivityDetailsController {
 
-    @GetMapping("/activity-details")
-    public String activityDetails() {
+    @Autowired
+    EquipmentService equipmentService;
 
+    @GetMapping("/activity-details")
+    public String activityDetails(Model model) {
+        model.addAttribute("getEquipment",equipmentService.getEquipment(1L));
         return "activityDetails";
 
     }
