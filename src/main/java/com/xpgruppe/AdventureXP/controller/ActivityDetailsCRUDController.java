19172dae256f05ef53c0b069a00659d11ded48d3
ZaksenDIku <zakariya.berrhi@gmail.com>
2026-03-10 15:10:08 +0100

    ActivityDetails.html rediger knap redirects to CRUDActivityDetails.html

--- src/main/java/com/xpgruppe/AdventureXP/controller/ActivityDetailsCRUDController.java
+++ src/main/java/com/xpgruppe/AdventureXP/controller/ActivityDetailsCRUDController.java
@@ -1,5 +1,24 @@
 package com.xpgruppe.AdventureXP.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import com.xpgruppe.AdventureXP.model.Activity;
+import com.xpgruppe.AdventureXP.service.ActivityService;
+import com.xpgruppe.AdventureXP.service.EquipmentService;
 
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
 public class ActivityDetailsCRUDController {
 
+    private final ActivityService activityService;
+
+    @GetMapping("/activity-details/edit")
+    public String activityDetails(@RequestParam Long id, Model model) {
+        Activity activity = activityService.getActivity(id);
+        model.addAttribute("activity", activity);
+            return "CRUDActivityDetails";
+    }
 }
